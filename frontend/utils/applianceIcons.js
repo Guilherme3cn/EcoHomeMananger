85b27e165eb55014bd50d9074ae17522f33e9c95
Guilherme <guilhermesampaio2009@hotmail.com>
2025-04-08 19:19:31 -0300

    atualizações dos icons

--- frontend/utils/applianceIcons.js
+++ frontend/utils/applianceIcons.js
@@ -1,7 +1,7 @@
 // utils/applianceIcons.js
 
 const applianceIcons = [
-  { name: 'Geladeira', source: require('../assets/fridge.png') },
+  { name: 'Geladeira', source: require('../assets/geladeira.png') },
   { name: 'Máquina de Lavar', source: require('../assets/maquina.png') },
   { name: 'Ar-condicionado', source: require('../assets/ar.png') },
   { name: 'Micro-ondas', source: require('../assets/microondas.png') },
@@ -11,6 +11,14 @@ const applianceIcons = [
   { name: 'tomada', source: require('../assets/tomada.png') },
   { name: 'disjuntor', source: require('../assets/disjuntor.png') },
   { name: 'chuveiro', source: require('../assets/chuveiro.png') },
+  { name: 'som', source: require('../assets/som.png') },
+  { name: 'notbook', source: require('../assets/notbook.png') },
+  { name: 'tomada2', source: require('../assets/tomada2.png') },
+  {name : 'celular', source: require('../assets/celular.png')},
+  {name : 'computer', source: require('../assets/computer.png')},
+  {name : 'tablet', source: require('../assets/tablet.png')},
+  {name : 'ventilador', source: require('../assets/ventilador.png')},
+
   
 
 ];
